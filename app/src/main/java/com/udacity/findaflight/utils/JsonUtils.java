67c73e58866efbd50be74ff3e890e8c1a6557fef
developerern <developerern@gmail.com>
2019-10-06 23:27:00 +0800

    added json and network utility classes

---
+++ app/src/main/java/com/udacity/findaflight/utils/JsonUtils.java
@@ -0,0 +1,15 @@
+package com.udacity.findaflight.utils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class JsonUtils {
+
+    public static List getFlightsByPrice(JSONObject recipeJsonObj) throws JSONException {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/udacity/findaflight/utils/NetworkUtils.java
@@ -0,0 +1,56 @@
+package com.udacity.findaflight.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public final class NetworkUtils {
+
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    public static URL buildUrl(String urlString) {
+        Uri builtUri = Uri.parse(urlString).buildUpon().build();
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URI: " + url);
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+}
