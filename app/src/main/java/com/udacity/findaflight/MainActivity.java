624eeb175eff2306dda5c21412a3651778b18b9e
developerern <developerern@gmail.com>
2019-10-07 22:35:00 +0800

    added DatePickerDialog and fixed radioGroup text color

--- app/src/main/java/com/udacity/findaflight/MainActivity.java
+++ app/src/main/java/com/udacity/findaflight/MainActivity.java
@@ -1,14 +1,82 @@
 package com.udacity.findaflight;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.udacity.findaflight.utils.DateUtils.getDateInDayMonthFormat;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.editTextReturnDate) EditText returnDate;
+    @BindView(R.id.textViewReturnDay) TextView returnDay;
+    @BindView(R.id.editTextDepartDate) EditText departDate;
+    @BindView(R.id.textViewDepartDay) TextView departDay;
+    Calendar calendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+        setOnClickListenerToDate(departDate);
+        setOnClickListenerToDate(returnDate);
     }
+
+    private void setOnClickListenerToDate(EditText date) {
+        date.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openDatePickerDialog(v);
+            }
+        });
+    }
+
+    private void openDatePickerDialog(View v) {
+        // Get Current Date
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                (view, year, monthOfYear, dayOfMonth) -> {
+                    String selectedDate = null;
+                    String dayMonth = null;
+                    String dayInWeek = null;
+
+                    try {
+                        selectedDate = getDateInDayMonthFormat(dayOfMonth, monthOfYear, year);
+                        String[] dateTokens = selectedDate.split(",");
+                        dayMonth = dateTokens[0];
+                        dayInWeek = dateTokens[1];
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    switch (v.getId()) {
+                        case R.id.editTextDepartDate:
+                            ((EditText) v).setText(dayMonth);
+                            ((EditText) v).setTextColor(Color.parseColor("#323232"));
+                            departDay.setText(dayInWeek);
+                            break;
+                        case R.id.editTextReturnDate:
+                            ((EditText) v).setText(dayMonth);
+                            ((EditText) v).setTextColor(Color.parseColor("#323232"));
+                            returnDay.setText(dayInWeek);
+                            break;
+                    }
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+
+
+        datePickerDialog.getDatePicker().setMinDate(calendar.getTimeInMillis());
+        datePickerDialog.show();
+    }
+
+
 }
---
+++ app/src/main/java/com/udacity/findaflight/utils/DateUtils.java
@@ -0,0 +1,20 @@
+package com.udacity.findaflight.utils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public final class DateUtils {
+
+    public static String getDateInDayMonthFormat(int day, int month, int year) throws ParseException {
+        DateFormat originalFormat = new SimpleDateFormat("d M yyyy", Locale.ENGLISH);
+        DateFormat targetFormat = new SimpleDateFormat("dd MMM,EEEE");
+        String originalDate = day + " " + (month + 1) + " " + year;
+        Date date = originalFormat.parse(originalDate);
+        String formattedDate = targetFormat.format(date).toUpperCase();
+
+        return formattedDate;
+    }
+}
