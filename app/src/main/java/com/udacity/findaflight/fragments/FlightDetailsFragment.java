ab532d4c9b7662e371a2dc97c8e3b9455e961f2f
developerern <developerern@gmail.com>
2019-10-17 23:32:55 +0800

    implemented CollapsingToolbarLayout for FlightDetailsFragment

---
+++ app/src/main/java/com/udacity/findaflight/fragments/FlightDetailsFragment.java
@@ -0,0 +1,46 @@
+package com.udacity.findaflight.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.udacity.findaflight.R;
+
+public class FlightDetailsFragment extends Fragment {
+
+    public FlightDetailsFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_flight_details, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
